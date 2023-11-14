8ad3b9ac3b91237b6d24bb9626edbf4c910962b2
lseong96 <lseong96@gmail.com>
2023-11-14 12:01:05 +0900

    ajax vue

--- Ch10/src/main/java/kr/ch10/controller/User1Controller.java
+++ Ch10/src/main/java/kr/ch10/controller/User1Controller.java
@@ -4,21 +4,34 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import kr.ch10.entity.User1Entity;
 import kr.ch10.service.User1Service;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
+@CrossOrigin("http://localhost:5173")
 @Controller
 public class User1Controller {
 	
 	@Autowired
 	private User1Service service;
+	
+	/*
+	 *	CORS(Cross-Origin Resource Sharing Policy)
+	 * 	- HTTP 웹 서비스 보안정책 중 하나로 동일 출처의 리소스만 공유하는 원칙(SOP)
+	 *  - @CrossOrigin 어노테이션 특징 Origin 주소를 설정하면 해당 Origin에서만 응답
+	 * 
+	 */
+	
 	
 	@ResponseBody
 	@GetMapping("/user1")
@@ -33,16 +46,19 @@ public class User1Controller {
 	public User1Entity user1(@PathVariable("id") String id) {
 		return service.selectUser1(id);
 	}
-	
+
 	@ResponseBody
 	@PostMapping("/user1")
-	public void register(User1Entity user1) {
+	public void register(@RequestBody User1Entity user1) {
+		
+		log.info(user1);
+		
 		service.insertUser1(user1);
 	}
 	
 	@ResponseBody
 	@PutMapping("/user1")
-	public void modify(User1Entity user1) {
+	public void modify(@RequestBody User1Entity user1) {
 		service.updateUser1(user1);
 	}
 	
